687c4a552c803dfe0d232c80cb64128733033a34
Arihant Joshi <arihant.joshi@infoworks.io>
2020-09-22 20:03:47 +0530

    Added Arguments to Update

--- updateCSV.py
+++ updateCSV.py
@@ -2,6 +2,9 @@
 
 import json
 import os.path
+import sys
+
+args = sys.argv()
 
 jsonPath = "./Data.json"
 data = dict([])
@@ -9,7 +12,35 @@ if(os.path.isfile(jsonPath)):
 	with open(jsonPath, "r") as f:
 		data = json.loads(f.read())
 
-data["key2"] = "val2"
+if(args[1] == "workspace"):
+	resource_group = args[2]
+	region = args[3]
+	environment_name = args[4]
+	db_private_subnet = args[5]
+	db_public_subnet = args[6]
+
+	if(resource_group not in data):
+		data[resource_group] = dict([])
+
+	data[resource_group]["region"] = region
+	data[resource_group]["WS Name"] = environment_name
+	data[resource_group]["db_private_subnet"] = environment_name
+	data[resource_group]["db_public_subnet"] = environment_name
+else:
+	resource_group = args[2]
+	region = args[3]
+	environment_name = args[4]
+	vm_machine_type = args[5]
+	os_version = args[6]
+
+	if(resource_group not in data):
+		data[resource_group] = dict([])
+
+	data[resource_group]["region"] = region
+	data[resource_group]["VM Name"] = environment_name
+	data[resource_group]["VM Type"] = vm_machine_type
+	data[resource_group]["OS Version"] = os_version
+
 
 with open(jsonPath,"w") as f:
 	f.write(json.dumps(data,indent=4))
